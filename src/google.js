334f9e12b35ca4cc5d87e87b9a00fde5c0ac711a
gh05d <pc@vipfy.com>
2021-02-26 19:48:06 +0100

    Split up script

--- src/google.js
+++ src/google.js
@@ -2,6 +2,7 @@ const Fs = require("fs");
 const Path = require("path");
 const Discord = require("discord.js");
 const { google } = require("googleapis");
+const readline = require("readline");
 const {
   client,
   POKER_TEXT_CHANNEL,
@@ -9,7 +10,13 @@ const {
 } = require("./constants");
 const { Tournament, PlayerTournament } = require("./sequelizeSetup");
 const { signupPlayers } = require("./helpers");
-const { authorize } = require("./authorize-google");
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = "token.json";
 
 let historyId = null;
 let channel = null;
@@ -33,7 +40,67 @@ module.exports = {
     clearInterval(client.interval);
     console.log("Stopped polling 🛑");
   },
+  authorize,
 };
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  // Check if we have previously stored a token.
+  Fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) {
+      return getNewToken(oAuth2Client, callback);
+    }
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
+  console.log("Authorize this app by visiting this url:", authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question("Enter the code from that page here: ", code => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        return console.error("Error retrieving access token", err);
+      }
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      Fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
+        if (err) return console.error(err);
+        console.log("Token stored to", TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
 
 /**
  * Fetches unread messages and returns the most current historyId
